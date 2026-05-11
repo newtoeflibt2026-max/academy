9894010d27aec6f05ffe1082290f5b609de37804
DR. Yamen <lyndmrh484@gmail.com>
2026-05-11 20:19:11 +0300

    fix: call init_db() on startup

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import asyncio, logging, sys
+﻿import asyncio, logging, sys
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from config import BOT_TOKEN
 from handlers import register_all
+from database import init_db
 
 async def main():
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
@@ -13,4 +14,5 @@ async def main():
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    init_db()
     asyncio.run(main())
